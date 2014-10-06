5b26aab88a117db421144a81ef38572558f54976
pikesley <github@orgraphone.org>
2014-10-06 15:58:52 +0100

    trololololol

--- features/name_that_researcher.feature
+++ features/name_that_researcher.feature
@@ -2,4 +2,5 @@ Feature: Name That Researcher
 
   Scenario: Show me the homepage
     When I go to the home page
-    Then I should see "Name That Researcher"
+    Then I should see "UnJ_6p6l.jpeg" within "img"
+    And I should see "J"
--- lib/name_that_researcher.rb
+++ lib/name_that_researcher.rb
@@ -1,8 +1,16 @@
 require 'sinatra/base'
+require 'haml'
 
 class NameThatResearcher < Sinatra::Base
+  @@names = File.readlines('names').map { |name| name.chomp }
   get '/' do
-    'Name That Researcher!'
+    haml :index, locals: {
+      title: 'Name That Researcher',
+      image: 'https://pbs.twimg.com/profile_images/481399996743303169/UnJ_6p6l.jpeg',
+      name: @@names.sample,
+      bootstrap_theme: 'http://bootswatch.com/superhero/bootstrap.min.css',
+      additional_styles: [ '../styles.css' ]
+    }
   end
 
   # start the server if ruby file executed directly
